e61dcce04c788dec4e070b8ee1d08cf93928c781
zcc <listenzcc@mail.bnu.edu.cn>
2020-03-06 12:50:43 +0800

    Daily update.

--- workshop/beta_distribution/beta_distribution.py
+++ workshop/beta_distribution/beta_distribution.py
@@ -23,7 +23,11 @@ def linspace(start, stop):
 # %%
 # Setup figure
 plt.style.use('ggplot')
-fig, axes = plt.subplots(2, 2, figsize=(8, 8))
+fig, axes = plt.subplots(3, 2, figsize=(8, 12))
+gs = axes[0][0].get_gridspec()
+for ax in axes[0]:
+    ax.remove()
+axbig = fig.add_subplot(gs[0, :])
 print()
 
 # %%
@@ -31,7 +35,7 @@ print()
 # alpha as a, beta as b.
 para_beta = [(2, 8), (20, 80), (200, 800), (5, 5), (50, 50), (80, 20)]
 
-ax = axes[0][0]
+ax = axbig
 
 for a, b in para_beta:
     label = f'Alpha={a}, Beta={b}'
@@ -39,13 +43,14 @@ for a, b in para_beta:
     curve = ax.plot(x_values, pdf, label=label)[0]
 
 ax.set_title('Beta distribution')
-ax.legend(loc='auto')
+ax.legend(loc='best')
 
 # %%
 # Draw mean and variance of Beta distribution with different parameters.
 start, stop = 3, 30
 a_values = linspace(start, stop)
 b_values = linspace(start, stop)
+x_grid, y_grid = np.meshgrid(a_values, b_values)
 
 # mean, var, skew, kurt
 mean = np.zeros((stop, stop))
@@ -58,23 +63,51 @@ for a in a_values:
         var[int(a)][int(b)] = v
 
 
-def fill_ax(mat, ax, title='title'):
+def imshow_ax(ax, mat, title='undefined'):
+    """ Fill [ax] with [mat] as [title] using imshow, [start] and [stop] are pre-defined parameters to control display range. """
+    # Imshow
     im = ax.imshow(mat)
-    ax.set_xlim([start, stop-1])
+    # Fine x-axis
+    ax.set_xlim([start, stop - 1])
+    ax.set_xlabel('Alpha')
+    # Fine y-axis
     ax.invert_yaxis()
-    ax.set_ylim([start, stop-1])
+    ax.set_ylim([start, stop - 1])
+    ax.set_ylabel('Beta')
+    # Add title
     ax.set_title(title)
-    fig.colorbar(im, extend='both', spacing='proportional', shrink=0.9, ax=ax)
+    # Add colorbar
+    fig.colorbar(im, ax=ax, extend='both', spacing='proportional', shrink=1.0)
     return im, ax
 
 
-im_mean, ax_mean = fill_ax(mean, axes[0][1], title='mean')
-im_var, ax_var = fill_ax(var, axes[1][1], title='var')
+def contourf_ax(ax, mat, title='undefined'):
+    """ Fill [ax] with [mat] as [title] using contourf, [x_grid] and [y_grid] are pre-defined parameters to control display range. """
+    # Contourf
+    im = ax.contourf(
+        x_grid,
+        y_grid,
+        mat[x_grid.astype(int), y_grid.astype(int)].transpose(),
+        10,
+        alpha=0.6)
+    ax.set_aspect(1)
+    ax.set_xlabel('Alpha')
+    ax.set_ylabel('Beta')
+    # Add colorbar
+    fig.colorbar(im, ax=ax, extend='both', spacing='proportional', shrink=1.0)
+    # Add title
+    ax.set_title(title)
+
+
+im_mean, ax_mean = imshow_ax(axes[1][0], mean, title='Mean')
+im_var, ax_var = imshow_ax(axes[2][0], var, title='Var')
+
+contourf_ax(axes[1][1], mean, title='Mean')
+contourf_ax(axes[2][1], var, title='Var')
 
 # %%
 # Show figure
 fig.tight_layout()
 fig.savefig('beta_distribution.png')
-fig
-
+plt.show()
 # %%
